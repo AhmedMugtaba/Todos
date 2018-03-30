0215e89770e0a1bdf7f22989728e309d2a94fb82
ahmedmugtaba <ahmedmugtaba88@gmail.com>
2018-03-30 13:11:03 -0400

    add Todo items

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ var todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'))
 
 
 app.get('/', function(req, res){
-    res.send("Hi there from the main routes")
+    res.sendFile('index.html')
 });
 
 app.use('/api/todos', todoRoutes);
